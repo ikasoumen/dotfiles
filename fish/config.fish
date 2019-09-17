9ef056fde7a00dfec84a162f3fd34767c8573156
ikasoumen <ikasoumen>
2019-09-17 15:15:50 +0900

    set umask

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,9 @@ eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 # nodenv 
 eval (nodenv init -)
+
+# Warning: umask is currently set to 000. Directories created by Homebrew cannot
+# be world-writable. This issue can be resolved by adding "umask 002" to
+# your ~/.config/fish/config.fish:
+#   echo 'umask 002' >> ~/.config/fish/config.fish
+umask 002
