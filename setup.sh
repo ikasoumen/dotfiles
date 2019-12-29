56869526967a7f9b860c735c9fb4abd2c5616d5b
ikasoumen <ikasoumen>
2019-12-30 00:04:10 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# apt
+sudo apt update
+sudo apt install build-essential
+
+# brew 
+brew upgrade
+brew install fish
+
+# ssh-keygen
+ssh-keygen -t ed25519
+
+# dotfiles
 mkdir -p ~/.config
 ln -sf ~/dev/dotfiles/fish ~/.config/
 ln -sf ~/dev/dotfiles/.gitconfig ~/
