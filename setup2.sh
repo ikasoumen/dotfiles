2f2535fa52c8ac8341cd89d13f13c47a1da45473
ikasoumen <ikasoumen>
2020-03-12 15:28:08 +0900

    update setup2.sh

--- setup2.sh
+++ setup2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ssh
-touch ~/.ssh
+mkdir ~/.ssh
 chmod 700 ~/.ssh
 
 touch ~/.ssh/id_rsa
@@ -14,20 +14,41 @@ touch ~/.ssh/config
 # apt 
 sudo apt update
 
-sudo apt install build-essential unzip
+sudo apt -y install build-essential unzip zip
 
-sudo apt install zsh 
+sudo apt -y install zsh 
 
-sudo apt install mysql-client mysql-server
+sudo apt -y install mysql-client mysql-server
+
+# dotfiles 
+rm /tmp/master.zip
+wget -O /tmp/master.zip https://github.com/ikasoumen/dotfiles/archive/master.zip
+unzip -d ~/ /tmp/master.zip
+mv ~/dotfiles-master ~/.dotfiles
 
 # sdkman 
 curl -s "https://get.sdkman.io" | bash
-# sdk list java
-# sdk install java 11.0.4.hs-adpt
-# sdk install sbt
+sdk list java
+sdk install java 8.0.242-open  
+sdk install sbt
 
 # .zshrc
 ln -sf ~/.dotfiles/.zshrc ~/
 
 # .gitconfig
 ln -sf ~/.dotfiles/.gitconfig ~/
+
+# mysql setup
+#
+# needs workaround hotfix
+# @see: https://github.com/wslutilities/wslu/issues/101
+# 
+# ```/etc/profile.d/wsl-integration.sh
+# if [[ "${HOME}" == "/" ]]; then
+#   exit 0
+# fi
+# ```
+
+# mysql: no root password
+sudo service mysql start
+sudo mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';"
